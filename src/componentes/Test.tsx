c195d355b88235d7a1b5f12e5b7129afcc71b4bd
Lucas Leite <luizcarlostb@hotmail.com>
2024-03-27 11:14:15 -0300

    initial commit

---
+++ src/componentes/Test.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+// const products = [
+//     { title: 'Cabbage', isFruit: false, id: 1 },
+//     { title: 'Garlic', isFruit: false, id: 2 },
+//     { title: 'Apple', isFruit: true, id: 3 },
+//   ];
+  
+//   export default function ShoppingList() {
+//     const listItems = products.map(product =>
+//       <li
+//         key={product.id}
+//         style={{
+//           color: product.isFruit ? 'magenta' : 'darkgreen'
+//         }}
+//       >
+//         {product.title}
+//       </li>
+//     );
+  
+//     return (
+//       <ul>{listItems}</ul>
+//     );
+//   }
+
+//   export default function MyApp() {
+//     return (
+//       <div>
+//         <h1>Counters that update separately</h1>
+//         <MyButton />
+//         <MyButton />
+//       </div>
+//     );
+//   }
+
+// ------------------------------------------------------------------------------------------------------------------------
+  
+export default function MyApp() {
+    const [count, setCount] = useState(0);
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
+  
+    return (
+      <div>
+        <h1>Counters that update together</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+      </div>
+    );
+  }
+  
+  function MyButton({ count, onClick }) {
+    return (
+      <button onClick={onClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
